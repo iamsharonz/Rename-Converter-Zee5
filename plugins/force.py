5c812b8889d5308d0edf872b9362575d302eb368
MrNobody503 <81288171+MrNobody503@users.noreply.github.com>
2021-03-30 23:41:13 +0530

    Update force.py

--- plugins/force.py
+++ plugins/force.py
@@ -55,12 +55,12 @@ async def text(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Support Channel', url='https://t.me/Nobodys_Bots'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/MrNobody_Here')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Other Bots', url='https://t.me/Nobodys_Bots'),
+                   
                 ]
             ]
         ),
